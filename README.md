62bd0a24cb35e418942c67a303ab6e21f6206414
Yanghwa <kellerj87@hotmail.com>
2017-02-01 11:55:58 -0500

    basic form

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# comp2068-lab4" 
+
+# this is the link :  https://dry-meadow-36734.herokuapp.com/
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+
+router.get('/junghwan', function(req, res, next) {
+  res.render('junghwan', { title: 'Junghwan Yang - Me', content: 'Hey Hey Hey' });
+});
+router.get('/sunok', function(req, res, next) {
+  res.render('sunok', { title: 'Sunok Kang - Mother', content: 'Hey Hey Hey' });
+});
+router.get('/younggyu', function(req, res, next) {
+  res.render('younggyu', { title: 'Younggyu Yang - Father', content: 'Hey Hey Hey' });
+});
+router.get('/heena', function(req, res, next) {
+  res.render('heena', { title: 'Heena Yang - Sister', content: 'Hey Hey Hey' });
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Main Page - Nothing', content: 'Hey Hey Hey' });
 });
 
 module.exports = router;
